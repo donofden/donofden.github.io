9370a194ee1e3055c0c77a3ca3d6c752b2c60f94
Aravind Kumar G <aravindkumar.ganesan@gmail.com>
2019-09-01 18:11:26 +0530

    Retrive Locations from JSON

--- travels/map.js
+++ travels/map.js
@@ -1,10 +1,10 @@
 function initMap() {
-    var zoomSize = 10;
+    var zoomSize = 3;
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: zoomSize,
         center: {
-            lat: -33.890542,
-            lng: 151.274856
+            lat: 38.9637, // Turkey
+            lng: 35.2433  // Turkey
         }
     });
     setMarker(map);
@@ -93,6 +93,7 @@ function initMap() {
 google.maps.event.addDomListener(window, 'load', initMap);
 
 function setMarker(map) {
+
     var beaches = [
         ['Bondi Beach', -33.890542, 151.274856, 4],
         ['Coogee Beach', -33.923036, 151.259052, 5],
@@ -111,85 +112,89 @@ function setMarker(map) {
         //scaledSize: new google.maps.Size(zoomSize * 2, zoomSize * 2)
     };
 
-    for (var i = 0; i < beaches.length; i++) {
-        var beach = beaches[i];
-        var marker = new google.maps.Marker({
-            position: {
-                lat: beach[1],
-                lng: beach[2]
-            },
-            map: map,
-            icon: image,
-            //shape: shape,
-            title: beach[0],
-            zIndex: beach[3]
-        });
-
-        // Information on the Marker Click
-        var contentString = '<div class="info-window">' +
-            '<h3>Info Window Content</h3>' +
-            '<div class="info-content">' +
-            '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>' +
-            '</div>' +
-            '</div>';
-
-        var infowindow = new google.maps.InfoWindow({
-            content: contentString,
-            maxWidth: 400
-        });
-        google.maps.event.addListener(marker, 'click', (function(marker, infowindow) {
-            return function() {
-                infowindow.open(map, marker);
-                // /windows.push(infowindow)
-                google.maps.event.addListener(map, 'click', function() {
-                    infowindow.close();
+    $.getJSON("position.json", function(data) {
+        $.each(data, function(index, position) {
+            for (var i = 0; i < position.length; i++) {
+                var location = position[i];
+                var marker = new google.maps.Marker({
+                    position: {
+                        lat: location['lat'],
+                        lng: location['lng']
+                    },
+                    map: map,
+                    icon: image,
+                    //shape: shape,
+                    title: location['title']
+                    //zIndex: beach[3]
                 });
-            };
-        })(marker, infowindow));
 
-        // Dynamically Resize the Icon with respect to Zoom
-        //when the map zoom changes, resize the icon based on the zoom level so the marker covers the same geographic area
-        google.maps.event.addListener(map, 'zoom_changed', function() {
-            var zoom = map.getZoom();
-            var icons = [
-                "icon_16X16.png", // Zoom - 0
-                "icon_20X20.png", // Zoom - 1
-                "icon_20X20.png", // Zoom - 2
-                "icon_24X24.png", // Zoom - 3
-                "icon_24X24.png", // Zoom - 4
-                "icon_32X32.png", // Zoom - 5
-                "icon_32X32.png", // Zoom - 6
-                "icon_32X32.png", // Zoom - 7
-                "icon_32X32.png", // Zoom - 8
-                "icon_32X32.png", // Zoom - 9
-                "icon_48X48.png", // Zoom - 10
-                "icon_48X48.png", // Zoom - 11
-                "icon_48X48.png", // Zoom - 12
-                "icon_48X48.png", // Zoom - 13
-                "icon_48X48.png", // Zoom - 14
-                "icon_64X64.png", // Zoom - 15
-                "icon_64X64.png", // Zoom - 16
-                "icon_64X64.png", // Zoom - 17
-                "icon_64X64.png", // Zoom - 18
-                "icon_64X64.png", // Zoom - 19
-                "icon_128X128.png", // Zoom - 20
-                "icon_128X128.png", // Zoom - 21
-                "icon_128X128.png" // Zoom - 22
-            ];
-            imageIconPath = "./marker/" + icons[zoom];
+                // Information on the Marker Click
+                var contentString = '<div class="info-window">' +
+                    '<h3>Info Window Content</h3>' +
+                    '<div class="info-content">' +
+                    '<p>' + location['title'] + 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>' +
+                    '</div>' +
+                    '</div>';
 
-            var image = {
-                //url: marker.getIcon().url,
-                url: imageIconPath,
-                // This marker is 20 pixels wide by 32 pixels high.
-                size: null,
-                // The origin for this image is (0, 0).
-                origin: null,
-                // The anchor for this image is the base of the flagpole at (0, 32).
-                anchor: null,
-                //scaledSize: new google.maps.Size(relativePixelSize, relativePixelSize) //changes the scale
-            };
-            marker.setIcon(image);
+                var infowindow = new google.maps.InfoWindow({
+                    content: contentString,
+                    maxWidth: 400
+                });
+                google.maps.event.addListener(marker, 'click', (function(marker, infowindow) {
+                    return function() {
+                        infowindow.open(map, marker);
+                        // /windows.push(infowindow)
+                        google.maps.event.addListener(map, 'click', function() {
+                            infowindow.close();
+                        });
+                    };
+                })(marker, infowindow));
+
+                // Dynamically Resize the Icon with respect to Zoom
+                //when the map zoom changes, resize the icon based on the zoom level so the marker covers the same geographic area
+                google.maps.event.addListener(map, 'zoom_changed', function() {
+                    var zoom = map.getZoom();
+                    var icons = [
+                        "icon_16X16.png", // Zoom - 0
+                        "icon_20X20.png", // Zoom - 1
+                        "icon_20X20.png", // Zoom - 2
+                        "icon_24X24.png", // Zoom - 3
+                        "icon_24X24.png", // Zoom - 4
+                        "icon_32X32.png", // Zoom - 5
+                        "icon_32X32.png", // Zoom - 6
+                        "icon_32X32.png", // Zoom - 7
+                        "icon_32X32.png", // Zoom - 8
+                        "icon_32X32.png", // Zoom - 9
+                        "icon_48X48.png", // Zoom - 10
+                        "icon_48X48.png", // Zoom - 11
+                        "icon_48X48.png", // Zoom - 12
+                        "icon_48X48.png", // Zoom - 13
+                        "icon_48X48.png", // Zoom - 14
+                        "icon_64X64.png", // Zoom - 15
+                        "icon_64X64.png", // Zoom - 16
+                        "icon_64X64.png", // Zoom - 17
+                        "icon_64X64.png", // Zoom - 18
+                        "icon_64X64.png", // Zoom - 19
+                        "icon_128X128.png", // Zoom - 20
+                        "icon_128X128.png", // Zoom - 21
+                        "icon_128X128.png" // Zoom - 22
+                    ];
+                    imageIconPath = "./marker/" + icons[zoom];
+
+                    var image = {
+                        //url: marker.getIcon().url,
+                        url: imageIconPath,
+                        // This marker is 20 pixels wide by 32 pixels high.
+                        size: null,
+                        // The origin for this image is (0, 0).
+                        origin: null,
+                        // The anchor for this image is the base of the flagpole at (0, 32).
+                        anchor: null,
+                        //scaledSize: new google.maps.Size(relativePixelSize, relativePixelSize) //changes the scale
+                    };
+                    marker.setIcon(image);
+                });
+            }
         });
-    }
+    });
 }
